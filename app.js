e11a65d1cfcccc5caca70d1deb823b98f8a9c61d
Davi Zancope <davizancope@gmail.com>
2023-03-28 09:55:07 -0300

    Assingment 4 solution

---
+++ app.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const users = [];
+
+app.get('/', (req, res, next) => {
+	res.render('add-name', { pageTitle: 'Add User' });
+});
+
+app.get('/users', (req, res, next) => {
+	res.render('users', { pageTitle: 'Users', users: users });
+});
+
+app.post('/add-user', (req, res, next) => {
+	users.push({ name: req.body.username });
+	res.redirect('/users');
+});
+
+app.listen(3000);
